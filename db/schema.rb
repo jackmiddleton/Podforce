a852786158888c940355fcf3731e44f2e18ecbe4
Marc Weitzmann <marc@weitzmann.org>
2021-05-29 13:52:10 +0200

    changed phone field into string, run seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_101925) do
+ActiveRecord::Schema.define(version: 2021_05_29_114647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_29_101925) do
   create_table "contacts", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.integer "phone"
+    t.string "phone"
     t.date "birth"
     t.string "company"
     t.string "email"
