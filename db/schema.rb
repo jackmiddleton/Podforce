713b30e0d8a5cf97866b157ac05d58fdeb2bddfe
Francisca Nunes <franciscaribeironunes@gmail.com>
2021-05-29 12:37:38 +0100

    Routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_29_101925) do
+ActiveRecord::Schema.define(version: 2021_05_29_110035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "phone"
+    t.date "birth"
+    t.string "company"
+    t.string "email"
+    t.text "note"
+    t.text "instagram"
+    t.text "twitter"
+    t.text "linkedin"
+    t.text "tag"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "emails", force: :cascade do |t|
+    t.bigint "contact_id", null: false
+    t.string "to"
+    t.text "subject"
+    t.text "content"
+    t.string "cc"
+    t.date "date_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contact_id"], name: "index_emails_on_contact_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.bigint "contact_id", null: false
+    t.bigint "user_id", null: false
+    t.text "description"
+    t.string "priority"
+    t.date "date_time"
+    t.string "ownership"
+    t.string "complete"
+    t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["contact_id"], name: "index_tasks_on_contact_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +73,7 @@ ActiveRecord::Schema.define(version: 2021_05_29_101925) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "emails", "contacts"
+  add_foreign_key "tasks", "contacts"
+  add_foreign_key "tasks", "users"
 end
