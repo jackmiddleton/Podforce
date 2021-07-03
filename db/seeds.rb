57f87aceca6422b50900031606999dabed35121d
Francisca Nunes <franciscaribeironunes@gmail.com>
2021-07-03 11:11:11 +0100

    solved seeds buf

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,17 +35,17 @@ task5 = Task.create!({contact: contact1, user: user3, description: "Send email w
 
 task6 = Task.create!({contact: contact2, user: user3, description: "Send email with invitation", priority: "High", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "email"})
 
-task7 = Task.create!({contact: contact1, user: user1, description: "Update Podcasts ideas Template", priority: "Low", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"}})
+task7 = Task.create!({contact: contact1, user: user1, description: "Update Podcasts ideas Template", priority: "Low", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"})
 
-task8 = Task.create!({contact: contact2, user: user2, description: "Update Podcasts ideas Template", priority: "Low", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"}})
+task8 = Task.create!({contact: contact2, user: user2, description: "Update Podcasts ideas Template", priority: "Low", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"})
 
-task9 = Task.create!({contact: contact1, user: user3, description: "Update Podcasts ideas Template", priority: "Low", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"}})
+task9 = Task.create!({contact: contact1, user: user3, description: "Update Podcasts ideas Template", priority: "Low", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"})
 
-task10 = Task.create!({contact: contact1, user: user1, description: "Zoom Meeting today with Sarah", priority: "High", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"}})
+task10 = Task.create!({contact: contact1, user: user1, description: "Zoom Meeting today with Sarah", priority: "High", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"})
 
-task11 = Task.create!({contact: contact2, user: user2, description: "Zoom Meeting today with Sarah", priority: "High", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"}})
+task11 = Task.create!({contact: contact2, user: user2, description: "Zoom Meeting today with Sarah", priority: "High", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"})
 
-task12 = Task.create!({contact: contact2, user: user3, description: "Zoom Meeting today with Sarah", priority: "High", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"}})
+task12 = Task.create!({contact: contact2, user: user3, description: "Zoom Meeting today with Sarah", priority: "High", date_time: DateTime.new(2022,2,3), ownership: "You", complete: false, category: "review"})
 
 
 puts "Creating 2 emails for each of the 3 users"
